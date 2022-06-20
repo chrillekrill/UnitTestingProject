626a9a683354532a11bbfcfcfd19848ada894167
Christoffer K <henricson21@hotmail.com>
2022-06-20 17:46:24 +0200

    I don't know what I am doing atm

---
+++ Testing/Controller/BaseControllerTest.cs
@@ -0,0 +1,25 @@
+﻿using System.Security.Claims;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Testing.Controller;
+
+public class BaseControllerTest : BaseTest
+{
+    protected ControllerContext SetupControllerContext()
+    {
+        var user = new ClaimsPrincipal(new ClaimsIdentity(new Claim[]
+        {
+            new Claim(ClaimTypes.NameIdentifier, Guid.NewGuid().ToString()),
+            new Claim(ClaimTypes.Email, "Chrille@chrillescompany.com")
+        }, "TestAuthentication"));
+
+        var controllerContext = new ControllerContext();
+        controllerContext.HttpContext = new DefaultHttpContext()
+        {
+            User = user
+        };
+
+        return controllerContext;
+    }
+}
--- Testing/Services/PricingServiceTests.cs
+++ Testing/Services/PricingServiceTests.cs
@@ -15,16 +15,113 @@ public class PricingServiceTests
     }
 
     [TestMethod]
-    public void First_test()
+    public void When_no_agreement_exists_on_customer_use_baseprice()
     {
-        var guid = Guid.NewGuid();
         var currentCustomer = new CurrentCustomerContext
         {
-            UserId = guid,
-            Email = "stefan@hejse",
             Agreements = new List<Agreement>()
         };
-        Mock<CurrentCustomerContext> cus = new Mock<CurrentCustomerContext>();
 
+        var products = new List<ProductServiceModel>()
+        {
+            new ProductServiceModel()
+            {
+                BasePrice = 1337
+            }
+        };
+
+        var result = _sut.CalculatePrices(products, currentCustomer);
+
+        Assert.AreEqual(1337, result.First().Price);
+    }
+
+    [TestMethod]
+    public void When_an_agreement_exists_use_agreement_discount()
+    {
+        var currentCustomer = new CurrentCustomerContext
+        {
+            Agreements = new List<Agreement>()
+            {
+                new Agreement
+                {
+                    AgreementRows = new List<AgreementRow>()
+                    {
+                        new AgreementRow
+                        {
+                            CategoryMatch = "van",
+                            PercentageDiscount = 10
+                        }
+                    }
+                }
+            }
+        };
+
+        var products = new List<ProductServiceModel>()
+        {
+            new ProductServiceModel()
+            {
+                BasePrice = 1337,
+                CategoryName = "van"
+            }
+        };
+
+        var result = _sut.CalculatePrices(products, currentCustomer);
+
+        Assert.AreEqual(1203, result.First().Price);
+    }
+
+    [TestMethod]
+    public void If_multiple_agreements_matches_the_same_product_the_one_with_the_highest_discount_should_be_used()
+    {
+        var currentCustomer = new CurrentCustomerContext
+        {
+            Agreements = new List<Agreement>()
+            {
+                new Agreement
+                {
+                    AgreementRows = new List<AgreementRow>()
+                    {
+                        new AgreementRow
+                        {
+                            CategoryMatch = "van",
+                            PercentageDiscount = 3
+                        },
+                        new AgreementRow
+                        {
+                            ProductMatch = "Hybrid",
+                            PercentageDiscount = 5
+                        }
+                    }
+                },
+                new Agreement
+                {
+                    AgreementRows = new List<AgreementRow>()
+                    {
+                        new AgreementRow
+                        {
+                            ManufacturerMatch = "Volvo",
+                            PercentageDiscount = 3
+                        }
+                    }
+                }
+            }
+            
+            
+
+        };
+        var products = new List<ProductServiceModel>()
+        {
+            new ProductServiceModel()
+            {
+                BasePrice = 1000,
+                CategoryName = "van",
+                Name = "Hybrid",
+                
+            }
+        };
+
+        var result = _sut.CalculatePrices(products, currentCustomer);
+
+        Assert.AreEqual(950, result.First().Price);
     }
 }
